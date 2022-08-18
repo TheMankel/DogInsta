db8fd8d8512c1ce1cd95b880fe03dd726fbcc74e
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-18 15:48:02 +0200

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config';
+import { DOG_API_URL, QUOTE_API_URL, USER_API_URL } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
@@ -7,10 +7,29 @@ export const state = {
   bookmarks: [],
 };
 
+const createPostData = function (userObject) {
+  state.post = {
+    username: userObject.login.username,
+    profilePictures: userObject.picture,
+    postImage: userObject.postImage,
+    quote: userObject.quote,
+  };
+};
+
 export const loadPost = async function () {
   try {
-    const data = await getJSON(API_URL);
-    state.post = data.message;
+    const imageData = await getJSON(DOG_API_URL);
+    const userData = await getJSON(USER_API_URL);
+    const quoteData = await getJSON(QUOTE_API_URL);
+
+    const { results } = userData;
+    const userObject = new Object(...results);
+    const { quote } = quoteData;
+
+    userObject.postImage = imageData.message;
+    userObject.quote = quote.body;
+
+    createPostData(userObject);
     // console.log(state.post);
   } catch (err) {
     console.error(`${err} 💥💥💥💥`);
