0ed4f4c170bc7e7410bcb91521ebbbebe14929f3
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-18 18:21:39 +0200

    Added new API

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,9 @@
-export const API_URL = 'https://dog.ceo/api/breeds/image/random';
+export const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random';
+export const USER_API_URL = 'https://randomuser.me/api/';
+export const QUOTE_API_URL = 'https://favqs.com/api/qotd';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 2;
 export const MODAL_CLOSE_SEC = 2.5;
 
 // API DOCS: https://dog.ceo/dog-api/
+// https://favqs.com/api
