b674c632d542b15edefd248b8cc34a3b242660d7
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-20 21:53:30 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,10 @@
 export const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random';
 export const USER_API_URL = 'https://randomuser.me/api/';
 export const QUOTE_API_URL = 'https://favqs.com/api/qotd';
+export const ALT_QUOTE_API_URL = 'https://quotable.io/quotes';
+export const MAX_LENGTH = 50;
+export const MIN_LENGTH = 30;
+export const MAX_PAGE_NUM = 65;
 export const NAT_ARR = 'au,br,ca,ch,de,dk,es,fi,fr,gb,ie,mx,nl,no,nz,ua,us';
 export const USER_DATA = 'name,picture';
 export const TIMEOUT_SEC = 10;
@@ -9,4 +13,5 @@ export const MODAL_CLOSE_SEC = 2.5;
 
 // RANDOM DOGP PHOTO API DOCS: https://dog.ceo/dog-api/
 // RANDOM USER API DOCS: https://randomuser.me/
+// ALTERNATIVE USER API DOCS: https://github.com/lukePeavey/quotable
 // RANDOM QUOTE API DOCS: https://favqs.com/api
