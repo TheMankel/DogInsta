99354a7732de16c9309953517036d4d87bf2bc62
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-20 19:04:12 +0200

    Clean up

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,22 +4,6 @@ import { FooterView } from './views/FooterView';
 import { PostView } from './views/PostView';
 import { LoaderView } from './views/LoaderView';
 
-// const controlPosts = async function () {
-//   PostView.renderSpinner();
-//   // Load new Post
-//   await model.loadPost();
-//   // Render new post
-//   PostView.render(model.state.post);
-
-//   // Add observer to last post and eventually generate new one
-//   PostView.addHandlerObserver(controlPosts);
-// };
-
-// const init = function () {
-//   PostView.addHandlerRender(controlPosts);
-// };
-// init();
-
 class Controller {
   constructor() {
     this.init();
