b8b68d0c7094ed67db829e763ec45fbb6b8216ed
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-20 21:54:05 +0200

    Changed fetching data from API

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,15 @@
 import {
+  ALT_QUOTE_API_URL,
   DOG_API_URL,
+  MAX_LENGTH,
+  MAX_PAGE_NUM,
+  MIN_LENGTH,
   NAT_ARR,
   QUOTE_API_URL,
   USER_API_URL,
   USER_DATA,
 } from './config';
-import { getJSON } from './helpers';
+import { getJSON, getRandomInt } from './helpers';
 
 export const state = {
   post: {},
@@ -15,28 +19,142 @@ export const state = {
 
 const createPostData = function (userObject) {
   state.post = {
-    // username: userObject.login.username,
     username: userObject.name.first,
     profilePictures: userObject.picture,
     postImage: userObject.postImage,
     description: userObject.description,
+    comments: userObject.comments,
+    likesCount: userObject.likesCount,
   };
 };
 
+// const createUserData = async function (amount = 1) {
+//   try {
+//     console.log('amount', amount);
+
+//     const userData = await getJSON(
+//       `${USER_API_URL}?inc=${USER_DATA}&nat=${NAT_ARR}&noinfo&results=${amount}`,
+//     );
+//     // const quoteData = await getJSON(QUOTE_API_URL);
+
+//     // const { results } = userData;
+//     // const { quote } = quoteData;
+
+//     // const userObject = new Object(...results);
+//     // userObject.description = quote.body;
+
+//     const { results } = userData;
+//     console.log(results);
+//     let userObject;
+//     // console.log([userObject].length);
+
+//     if (results.length === 1) {
+//       const quoteData = await getJSON(QUOTE_API_URL);
+
+//       const { quote } = quoteData;
+
+//       userObject = new Object(...results);
+//       console.log(userObject);
+//       userObject.description = quote.body;
+//     } else {
+//       userObject = results.map((res) => new Object(res));
+//       console.log(userObject);
+
+//       for (let i = 0; i < amount; i++) {
+//         const quoteData = await getJSON(QUOTE_API_URL);
+//         const { quote } = quoteData;
+//         userObject[i].description = quote.body;
+//       }
+//     }
+
+//     return userObject;
+//   } catch (err) {
+//     console.error(`${err} 💥💥💥💥`);
+//     throw err;
+//   }
+// };
+
+// export const loadPost = async function () {
+//   try {
+//     // const userData = await getJSON(
+//     //   `${USER_API_URL}?inc=${USER_DATA}&nat=${NAT_ARR}&noinfo`,
+//     // );
+//     // const imageData = await getJSON(DOG_API_URL);
+//     // const quoteData = await getJSON(QUOTE_API_URL);
+
+//     // const { results } = userData;
+//     // const { message } = imageData;
+//     // const { quote } = quoteData;
+
+//     // const userObject = new Object(...results);
+//     // userObject.postImage = message;
+//     // userObject.description = quote.body;
+
+//     const imageData = await getJSON(DOG_API_URL);
+//     const { message } = imageData;
+
+//     const userObject = await createUserData();
+//     userObject.postImage = message;
+//     userObject.comments = [];
+//     userObject.likesCount = getRandomInt(0, 10000);
+
+//     const commentsCount = getRandomInt(1, 3);
+
+//     // for (let i = 0; i < commentsCount; i++) {
+//     //   // const quoteData = await getJSON(QUOTE_API_URL);
+//     //   // const { quote } = quoteData;
+
+//     //   // userObject.comments.push(quote.body);
+
+//     //   const userComment = await createUserData();
+//     //   userObject.comments.push(userComment);
+//     // }
+//     const userComment = await createUserData(2);
+//     userObject.comments = userComment;
+
+//     createPostData(userObject);
+//     console.log(state.post);
+//   } catch (err) {
+//     console.error(`${err} 💥💥💥💥`);
+//     throw err;
+//   }
+// };
+
+// export const addBookmark = function (bookmark) {
+//   state.bookmarks.push(bookmark);
+// };
+
 export const loadPost = async function () {
   try {
-    const imageData = await getJSON(DOG_API_URL);
-    const userData = await getJSON(
-      `${USER_API_URL}?inc=${USER_DATA}&nat=${NAT_ARR}&noinfo`,
+    const randNum = getRandomInt(2, 5);
+    const pageNum = getRandomInt(1, MAX_PAGE_NUM);
+
+    const usersData = await getJSON(
+      `${USER_API_URL}?inc=${USER_DATA}&nat=${NAT_ARR}&noinfo&results=${randNum}`,
     );
-    const quoteData = await getJSON(QUOTE_API_URL);
 
-    const { results } = userData;
-    const userObject = new Object(...results);
-    const { quote } = quoteData;
+    const quotesData = await getJSON(
+      `${ALT_QUOTE_API_URL}?page=${pageNum}&limit=${randNum}&maxLength=${MAX_LENGTH}&minLength=${MIN_LENGTH}`,
+    );
 
-    userObject.postImage = imageData.message;
-    userObject.description = quote.body;
+    const imageData = await getJSON(DOG_API_URL);
+
+    const resultsUsers = usersData.results;
+    const resultsQuotes = quotesData.results;
+    const { message } = imageData;
+    const comments = resultsUsers.map((user, i) => {
+      user.comment = resultsQuotes[i].content;
+      return user;
+    });
+
+    // console.log(resultsUsers);
+    // console.log(resultsQuotes);
+
+    const userObject = resultsUsers.shift();
+    userObject.description = resultsQuotes.shift().content;
+    userObject.postImage = message;
+    userObject.comments = comments;
+    userObject.likesCount = getRandomInt(0, 10000);
 
     createPostData(userObject);
     // console.log(state.post);
@@ -45,7 +163,3 @@ export const loadPost = async function () {
     throw err;
   }
 };
-
-// export const addBookmark = function (bookmark) {
-//   state.bookmarks.push(bookmark);
-// };
