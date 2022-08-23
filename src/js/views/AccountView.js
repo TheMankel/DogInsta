65da241aa7e8327a62b37a3a051b4a4005485368
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-23 18:49:24 +0200

    Update AccountView.js

--- src/js/views/AccountView.js
+++ src/js/views/AccountView.js
@@ -1,6 +1,8 @@
 import View from './View';
+import icons from '../../img/icons.svg';
 
 export class AccountView extends View {
+  _parentElement = document.querySelector('.container');
   _message = 'This function is unsupported. Have some balloons 🎈🎈';
 
   constructor() {
@@ -22,5 +24,36 @@ export class AccountView extends View {
     // fallowers: '21',
     // following: '37',
     // bookmarks: [],
+  }
+
+  _generateMarkup() {
+    return `
+      <header class="header hidden">
+        <div class="header__account">
+          <span class="header__account-username">${this._username}</span>
+        </div>
+        <nav class="nav">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <button class="btn-tiny nav__btn--add-post">
+                <svg>
+                  <use href="${icons}#icon-add"></use>
+                </svg>
+              </button>
+            </li>
+            <li class="nav__item">
+              <button class="btn-tiny nav__btn--bookmarks-show">
+                <svg>
+                  <use href="${icons}#icon-bookmark"></use>
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main class="account hidden">
+
+      </main>
+    `;
   }
 }
