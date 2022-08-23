7fd704f43c642fe362ec28641398910f97ce0a67
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-23 14:17:37 +0200

    Update HeaderView.js

--- src/js/views/HeaderView.js
+++ src/js/views/HeaderView.js
@@ -2,6 +2,7 @@ import View from './View';
 import icons from '../../img/icons.svg';
 import { USER_IMAGES } from '../config';
 import { PostView } from './PostView';
+import { GalleryView } from './GalleryView';
 
 export class HeaderView extends View {
   _parentElement = document.querySelector('.container');
@@ -23,163 +24,165 @@ export class HeaderView extends View {
     if (!btn) return;
 
     if (btn.classList.contains('nav__btn--add-post')) {
-      this.render(USER_IMAGES, 'afterbegin');
-      this.#initPostPhotoButtons();
+      const gallery = new GalleryView(USER_IMAGES, 'afterbegin');
+      gallery.addHandlerAccount(this._account);
+      // this.render(USER_IMAGES, 'afterbegin');
+      // this.#initPostPhotoButtons();
     } else this.renderMessage();
   }
 
-  #selectPhotoHandler(modal, e) {
-    const images = modal.querySelectorAll('img');
-    const selectedImg = e.target.closest('img');
+  // #selectPhotoHandler(modal, e) {
+  //   const images = modal.querySelectorAll('img');
+  //   const selectedImg = e.target.closest('img');
 
-    if (!selectedImg || !images.length) return;
+  //   if (!selectedImg || !images.length) return;
 
-    images.forEach((img) => img.classList.remove('active'));
+  //   images.forEach((img) => img.classList.remove('active'));
 
-    selectedImg.classList.toggle('active');
-    this._selectedImgSrc = selectedImg.src;
-  }
+  //   selectedImg.classList.toggle('active');
+  //   this._selectedImgSrc = selectedImg.src;
+  // }
 
-  #initPostPhotoButtons() {
-    const modal = document.querySelector('.modal__gallery');
-    const nav = modal.querySelector('.modal__nav');
-    const modalPhotos = modal.querySelector('.modal__photos');
-    this._selectedImgSrc = '';
+  // #initPostPhotoButtons() {
+  //   const modal = document.querySelector('.modal__gallery');
+  //   const nav = modal.querySelector('.modal__nav');
+  //   const modalPhotos = modal.querySelector('.modal__photos');
+  //   this._selectedImgSrc = '';
 
-    modal.querySelector('.btn--cancel').addEventListener('click', function () {
-      modal.remove();
-    });
+  //   modal.querySelector('.btn--cancel').addEventListener('click', function () {
+  //     modal.remove();
+  //   });
 
-    modalPhotos.addEventListener(
-      'click',
-      this.#selectPhotoHandler.bind(this, modal),
-    );
+  //   modalPhotos.addEventListener(
+  //     'click',
+  //     this.#selectPhotoHandler.bind(this, modal),
+  //   );
 
-    modal
-      .querySelector('.btn--arrow-post')
-      .addEventListener(
-        'click',
-        this.#processImage.bind(this, modalPhotos, modal, nav),
-      );
-  }
+  //   modal
+  //     .querySelector('.btn--arrow-post')
+  //     .addEventListener(
+  //       'click',
+  //       this.#processImage.bind(this, modalPhotos, modal, nav),
+  //     );
+  // }
 
-  #processImage(modalPhotos, modal, nav) {
-    if (this._selectedImgSrc) {
-      modalPhotos.remove();
-      modal.querySelector('.modal__bottom').remove();
+  // #processImage(modalPhotos, modal, nav) {
+  //   if (this._selectedImgSrc) {
+  //     modalPhotos.remove();
+  //     modal.querySelector('.modal__bottom').remove();
 
-      const markupPhotoToPost = this.#generateMarkupPhotoToPost();
-      const markupBtnCheck = this.#generateMarkupBtnCheck();
+  //     const markupPhotoToPost = this.#generateMarkupPhotoToPost();
+  //     const markupBtnCheck = this.#generateMarkupBtnCheck();
 
-      nav.insertAdjacentHTML('afterend', markupPhotoToPost);
-      nav.insertAdjacentHTML('beforeend', markupBtnCheck);
+  //     nav.insertAdjacentHTML('afterend', markupPhotoToPost);
+  //     nav.insertAdjacentHTML('beforeend', markupBtnCheck);
 
-      modal
-        .querySelector('.btn--check')
-        .addEventListener('click', this.#addNewPost.bind(this, modal));
+  //     modal
+  //       .querySelector('.btn--check')
+  //       .addEventListener('click', this.#addNewPost.bind(this, modal));
 
-      modal.querySelector('.btn--arrow-post').remove();
-    }
-  }
+  //     modal.querySelector('.btn--arrow-post').remove();
+  //   }
+  // }
 
-  #addNewPost(modal) {
-    const input = modal.querySelector('.modal__bottom-description');
-    const description = input.value.trim();
+  // #addNewPost(modal) {
+  //   const input = modal.querySelector('.modal__bottom-description');
+  //   const description = input.value.trim();
 
-    if (!description) {
-      input.value = '';
-      return;
-    }
+  //   if (!description) {
+  //     input.value = '';
+  //     return;
+  //   }
 
-    const data = this.#generatePostDataObject(description);
-    const post = new PostView(data, 'afterbegin');
+  //   const data = this.#generatePostDataObject(description);
+  //   const post = new PostView(data, 'afterbegin');
 
-    post.addHandlerAccount(this._account);
+  //   post.addHandlerAccount(this._account);
 
-    //DECIDE POSTVIEW OR ITS DATA
-    this._account._posts.push(post);
+  //   //DECIDE POSTVIEW OR ITS DATA
+  //   this._account._posts.push(post);
 
-    modal.remove();
-  }
+  //   modal.remove();
+  // }
 
-  #generatePostDataObject(description) {
-    return {
-      username: this._account._username,
-      picture: this._account._profilePicture,
-      postImage: this._selectedImgSrc,
-      description: description,
-      comments: [],
-      likesCount: 0,
-    };
-  }
+  // #generatePostDataObject(description) {
+  //   return {
+  //     username: this._account._username,
+  //     picture: this._account._profilePicture,
+  //     postImage: this._selectedImgSrc,
+  //     description: description,
+  //     comments: [],
+  //     likesCount: 0,
+  //   };
+  // }
 
-  #generateMarkupPhotoToPost() {
-    return `
-    <section class="modal__post">
-      <div class="modal__post-photo">
-        <img
-          src="${this._selectedImgSrc}"
-          alt="Post photo" />
-      </div>
-    </section>
-    <div class="modal__bottom--input">
-      <img
-        src="${this._account._profilePicture}"
-        alt="Profile picture" />
-      <input class="modal__bottom-description" placeholder="Add description..." type="text" autocorrect="off" autocomplete="off"></input>
-    </div>
-    `;
-  }
+  // #generateMarkupPhotoToPost() {
+  //   return `
+  //   <section class="modal__post">
+  //     <div class="modal__post-photo">
+  //       <img
+  //         src="${this._selectedImgSrc}"
+  //         alt="Post photo" />
+  //     </div>
+  //   </section>
+  //   <div class="modal__bottom--input">
+  //     <img
+  //       src="${this._account._profilePicture}"
+  //       alt="Profile picture" />
+  //     <input class="modal__bottom-description" placeholder="Add description..." type="text" autocorrect="off" autocomplete="off"></input>
+  //   </div>
+  //   `;
+  // }
 
-  #generateMarkupBtnCheck() {
-    return `
-    <button class="btn-tiny btn--check">
-      <svg>
-        <use href="${icons}#icon-check"></use>
-      </svg>
-    </button>
-    `;
-  }
+  // #generateMarkupBtnCheck() {
+  //   return `
+  //   <button class="btn-tiny btn--check">
+  //     <svg>
+  //       <use href="${icons}#icon-check"></use>
+  //     </svg>
+  //   </button>
+  //   `;
+  // }
 
-  #generateGalleryImages() {
-    let markup = ``;
-    for (let i = 0; i < this._data.length; i++) {
-      markup += `
-                <div class="modal__photos-img">
-                  <img
-                  src="${this._data[i]}"
-                  alt="Gallery photo" />
-                </div>
-              `;
-    }
-    // console.log(markup);
-    return markup;
-  }
+  // #generateGalleryImages() {
+  //   let markup = ``;
+  //   for (let i = 0; i < this._data.length; i++) {
+  //     markup += `
+  //               <div class="modal__photos-img">
+  //                 <img
+  //                 src="${this._data[i]}"
+  //                 alt="Gallery photo" />
+  //               </div>
+  //             `;
+  //   }
+  //   // console.log(markup);
+  //   return markup;
+  // }
 
-  _generateMarkup() {
-    return `
-      <div class="modal__gallery">
-        <nav class="modal__nav">
-          <button class="btn-tiny btn--cancel">
-            <svg>
-              <use href="${icons}#icon-cancel"></use>
-            </svg>
-          </button>
-          <span>New post</span>
-          <button class="btn-tiny btn--arrow-post">
-            <svg>
-              <use href="${icons}#icon-arrow-post"></use>
-            </svg>
-          </button>
-        </nav>
-        <section class="modal__photos">
-          <div class="modal__photos-wrapper">
-           ${this.#generateGalleryImages()}
-          </div>
-        </section>
-        <div class="modal__bottom">
-        </div>
-      </div>
-    `;
-  }
+  // _generateMarkup() {
+  //   return `
+  //     <div class="modal__gallery">
+  //       <nav class="modal__nav">
+  //         <button class="btn-tiny btn--cancel">
+  //           <svg>
+  //             <use href="${icons}#icon-cancel"></use>
+  //           </svg>
+  //         </button>
+  //         <span>New post</span>
+  //         <button class="btn-tiny btn--arrow-post">
+  //           <svg>
+  //             <use href="${icons}#icon-arrow-post"></use>
+  //           </svg>
+  //         </button>
+  //       </nav>
+  //       <section class="modal__photos">
+  //         <div class="modal__photos-wrapper">
+  //          ${this.#generateGalleryImages()}
+  //         </div>
+  //       </section>
+  //       <div class="modal__bottom">
+  //       </div>
+  //     </div>
+  //   `;
+  // }
 }
