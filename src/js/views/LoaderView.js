13909ac2ae46381c03513fa4a75d0d355e1fb1e6
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-24 14:53:48 +0200

    Refactor LoaderView

--- src/js/views/LoaderView.js
+++ src/js/views/LoaderView.js
@@ -1,32 +1,36 @@
+import View from './View';
 import icons from '../../img/icons.svg';
 
-export class LoaderView {
+export class LoaderView extends View {
   _parentElement = document.querySelector('.posts');
-  // _posts = [...document.querySelectorAll('.post')];
 
   constructor() {
-    // super();
-    // this._parentElement = this._posts.at(-1);
-    this.renderSpinner();
+    super();
+
+    this._data = 'null';
+    this.render('beforeend');
   }
 
-  renderSpinner() {
-    const markup = `
+  // renderSpinner() {
+  //   const markup =
+  //     // const markup = `
+  //     //   <div class="loading"></div>
+  //     // `;
+
+  //     this._parentElement.insertAdjacentHTML('beforeend', markup);
+  // }
+
+  destroy() {
+    document.querySelector('.loading__spinner').remove();
+  }
+
+  _generateMarkup() {
+    return `
       <div class='loading__spinner'>
         <svg class="loading__spinner-icon">
           <use class="path" href="${icons}#icon-spinner"></use>
         </svg>
       </div>
     `;
-
-    // const markup = `
-    //   <div class="loading"></div>
-    // `;
-
-    this._parentElement.insertAdjacentHTML('beforeend', markup);
-  }
-
-  destroy() {
-    document.querySelector('.loading__spinner').remove();
   }
 }
