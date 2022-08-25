54951ae6300186898779e0c5e2ed48c2a50effd4
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-25 20:30:49 +0200

    Update AccountView

--- src/js/views/AccountView.js
+++ src/js/views/AccountView.js
@@ -25,7 +25,19 @@ export class AccountView extends View {
     // following: '37',
     // bookmarks: [],
   }
+  #generatePhotos() {
+    const markup = this._account.posts
+      .map((post) => {
+        return `<div class="profile__posts-img">
+        <img
+        src="${post._data.postImage}"
+        alt="Gallery photo" />
+      </div> `;
+      })
+      .join('');
 
+    return markup;
+  }
   _generateMarkup() {
     // <img
     // src="${this._account.posts[0]._data.postImage}"
@@ -35,7 +47,9 @@ export class AccountView extends View {
       <div class="account hidden">
         <header class="header">
           <div class="header__account">
-            <span class="header__account-username">${this._account.username}</span>
+            <span class="header__account-username">${
+              this._account.username
+            }</span>
           </div>
           <nav class="nav">
             <ul class="nav__list">
@@ -58,27 +72,32 @@ export class AccountView extends View {
         </header>
         <main class="profile">
           <div class="profile__data">
-            <div class="profile__data-user">
-              <img
-                src="${this._account.profilePicture}"
-                alt="Account icon"
-                class="profile__data-img" />
-              <p class="profile__data-name">${this._account.fullname}</p>
+            <div class="profile__data-top">
+              <div class="profile__data-picture">
+                <img
+                  src="${this._account.profilePicture}"
+                  alt="Account icon"
+                  class="profile__data-img" />
+              </div>
+              <ul class="profile__data-list">
+                <li class="profile__data-item">
+                  <span>${this._account.posts.length}</span>
+                  <p>Posts</p>
+                </li>
+                <li class="profile__data-item">
+                  <span>${this._account.followers}</span>
+                  <p>Followers</p>
+                </li>
+                <li class="profile__data-item">
+                  <span>${this._account.following}</span>
+                  <p>Following</p>
+                </li>
+              </ul>
             </div>
-            <ul class="profile__data-list">
-              <li class="profile__data-item">
-                <span>${this._account.posts.length}</span>
-                <p>Posts</p>
-              </li>
-              <li class="profile__data-item">
-                <span>${this._account.followers}</span>
-                <p>Followers</p>
-              </li>
-              <li class="profile__data-item">
-                <span>${this._account.following}</span>
-                <p>Following</p>
-              </li>
-            </ul>
+            <div class="profile__data-info">
+              <p class="profile__data-name">${this._account.fullname}</p>
+              <p class="profile__data-bio">${this._account.bio}</p>
+            </div>
           </div>
           <div class="profile__posts">
             <nav class="profile__posts-nav">
@@ -100,9 +119,7 @@ export class AccountView extends View {
               </ul>
             </nav>
             <div class="profile__posts-wrapper">
-              <div class="profile__posts-img">
-                
-              </div>
+              ${this.#generatePhotos() || 'You have no posts yet'}
             </div>
           </div>
         </main>
